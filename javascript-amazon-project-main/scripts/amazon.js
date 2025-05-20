735815638485c90ef1743f5c9440781295ec7a6e
ervnmrcdo <ervinmercado31@gmail.com>
2025-05-20 16:08:43 +0800

    Add to cart

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -46,11 +46,32 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">Add to Cart</button>
+          <button class="add-to-cart-button button-primary"
+          data-product-id="${product.id}"
+          >Add to Cart</button>
         </div>
 
 `;
 });
-console.log(productsHTML);
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelectorAll(".add-to-cart-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    const quantity = button.dataset.productId;
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({ productId, quantity: 1 });
+    }
+    console.log(cart);
+  });
+});
