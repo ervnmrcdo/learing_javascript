df6be94f98477175470d76ae395e15148e44f908
ervnmrcdo <ervinmercado31@gmail.com>
2025-06-13 21:09:34 +0800

    Save data and generate HTML for delivery options

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,20 +1,19 @@
 export let cart = JSON.parse(localStorage.getItem("cart"));
 
 if (!cart) {
-  console.log("hello world");
   cart = [
     {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 1,
+      deliveryOptionId: "1",
     },
     {
       id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
       quantity: 2,
+      deliveryOptionId: "2",
     },
   ];
 }
-
-console.log(cart);
 
 const saveToStorage = () => {
   localStorage.setItem("cart", JSON.stringify(cart));
@@ -33,7 +32,7 @@ export const addToCart = (product) => {
   if (matchingItem) {
     matchingItem.quantity += 1;
   } else {
-    cart.push({ id: product.productId, quantity: 1 });
+    cart.push({ id: product.productId, quantity: 1, deliveryOptionId: 1 });
   }
   console.log(cart);
   saveToStorage();
